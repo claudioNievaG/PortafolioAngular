06fb00d8f786164bc0e47e846171679dcc06420c
Claudio Nieva <claudionieva.g@gmail.com>
2023-04-06 20:44:13 -0300

    proyecto base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { AppFooterComponent } from './footer/app-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
