1ff7a0e86ab7eea3f9172df9b43d2cd85e6e49eb
Claudio Nieva <claudionieva.g@gmail.com>
2023-04-07 18:59:18 -0300

    agregando funcionalidad al header.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -8,8 +10,25 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 constructor(){}
 
+@HostListener('window:scroll', ['$event'])
+onWindowScroll() {
+  const header = document.querySelector('header') as HTMLElement;;
+  const headerHeight = header?.offsetHeight;
+  const scrolledPastHeader = window.scrollY > headerHeight;
+
+  if (scrolledPastHeader) {
+    console.clear();
+    console.log('window.scrollY', window.scrollY);
+    console.log('headerHeight', headerHeight);
+
+  }else{
+    console.clear();
+    console.log('window.scrollY', window.scrollY);
+    console.log('headerHeight', headerHeight);
+  }
+}
+
 ngOnInit(): void {
- console.log('hola soy claudio');
 
 }
 }
