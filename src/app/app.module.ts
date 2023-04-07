06807bcc02b0ce782c830fc7711fe18b990df60c
Claudio Nieva <claudionieva.g@gmail.com>
2023-04-07 18:58:46 -0300

    asignacion de los componentes en el modulo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { AppFooterComponent } from './footer/app-footer.component';
+import { MyProjectsComponent } from './main/my-projects/my-projects.component';
+import { TechnicalSkillsComponent } from './main/technical-skills/technical-skills.component';
+import { ResumenComponent } from './main/resumen/resumen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    AppFooterComponent
+    AppFooterComponent,
+    MyProjectsComponent,
+    TechnicalSkillsComponent,
+    ResumenComponent
   ],
   imports: [
     BrowserModule,
